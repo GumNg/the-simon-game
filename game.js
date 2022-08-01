3ededf1da7531d91882e8d9b6daafa45990b2782
Gum Ng <jon9609@gmail.com>
2022-08-01 13:21:45 -0700

    changed variable name inside function

--- game.js
+++ game.js
@@ -3,6 +3,6 @@ let buttonColors = ["red", "blue", "green", "yellow"];
 
 // function to generate next pattern sequence
 function nextSequence() {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-    return randomNumber
+    let rng = Math.floor(Math.random() * 3) + 1;
+    return rng
 };
