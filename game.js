a0876735ae0ab7f3b779c2dbb067e48c7983da0e
Gum Ng <jon9609@gmail.com>
2022-08-01 15:08:26 -0700

    created selector with callback to select file following random color generator and play corresponding sound

--- game.js
+++ game.js
@@ -22,3 +22,9 @@ gamePattern.push(randomChosenColor);
 // animation for random color button to flash when clicked
 $("#" + randomChosenColor).on("click", function() {
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100)});
+
+// random color button plays sound when clicked corresponding to color
+$("#" + randomChosenColor).on("click", function() {
+    let randomSound = new Audio("sounds/" + randomChosenColor + ".mp3");
+    randomSound.play();
+})
