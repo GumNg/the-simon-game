75a915cd3c756c557c6ef0ef2738b54f6e0d2767
Gum Ng <jon9609@gmail.com>
2022-08-01 13:56:50 -0700

    created empty array to store color generated from randomChosenColor

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 // array to hold sequence of colors
 let buttonColors = ["red", "blue", "green", "yellow"];
 
+// array to store color chosen from RandomChoseColor 
+let gamePattern = [];
+
 // function to generate next pattern sequence
 function nextSequence() {
     let rng = Math.floor(Math.random() * 3) + 1;
